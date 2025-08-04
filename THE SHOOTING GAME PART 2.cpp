12ae61d66fde31ecb0af437dfca7e45c9a7d5707
GodfredBansah <godfredbansah73@gmail.com>
2025-08-04 20:02:05 +0000

    Add files via upload

---
+++ THE SHOOTING GAME PART 2.cpp
@@ -0,0 +1,5 @@
+# include<iostream>
+using namespace std;
+int main(){cout<<"hello world let me have my answer pls";
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,20 +1,82 @@
 #include <iostream>
-
+#include<conio.h>
+#include<windows.h>
 using namespace std;
-class Student{
-    public:
-    string name;
-    int age;
-    double gpa;
-Student(string x,int y,double z){
-name=x;
-age=y;
-gpa=z;
-}
-};
-int main(){  Student student1("sponge",28,4.23);
-cout<<student1.name<<endl;
-cout<<student1.age<<endl;
-cout<<student1.gpa<<endl;
-    return 0;
-}
+bool gameOver;
+const int width=20;
+const int height=20;
+int x,y,obstacleX,obstacleY;
+int score;
+int speed=100;
+char car='A';
+
+ void Setup(){
+ gameOver=false;
+ x=width/2;
+ y=height-1;
+ obstacleX=rand()%width;
+ obstacleY=0;
+
+
+
+ }
+ void Draw(){
+ system("cls");
+ for (int i=0;i<width+2;i++)
+    cout<<"H";
+    cout<<endl;
+
+ for (int i=0;i<height;i++){
+    cout<<"H";
+    for(int j=0;j<width;j++){
+        if (j==x&&i==y)
+            cout<<car;
+        else if(j==obstacleX&&i==obstacleY)
+            cout<<"OO";
+        else cout<<" ";
+
+    }
+    cout<<"H"<<endl;
+ }
+ for(int i=0;i<width+2;i++)
+    cout<<"H";
+ cout<<"\nScore:"<<score<<endl;
+
+
+ }
+ void Input(){
+ if(_kbhit()){
+    switch(_getch()){
+    case 'a':if(x>0)x--;break;
+    case 'd':if(x<width-1)x++;
+    break;
+    case 'x':gameOver=true;break;
+    }
+ }
+ }
+ void Logic(){
+ obstacleY++;
+ if (obstacleY>height){
+    obstacleY=0;
+    obstacleX=rand()%width;
+    score++;
+    if (speed>20)speed-=2;
+ }
+ if (obstacleX==x&&obstacleY==y){
+    gameOver=true;
+ }
+ }
+ int main(){
+ Setup();
+ while(!gameOver){
+    Draw();
+    Input();
+    Logic();
+    Sleep(speed);
+ }cout<<"\nGame Over!Final Score:"<<score<<endl;
+ system("pause");
+ return 0;
+ }
+
+
+
